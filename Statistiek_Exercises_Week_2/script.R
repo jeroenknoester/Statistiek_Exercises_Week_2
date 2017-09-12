e9641c56dbc118eadb6203ad70cf863da8284866
Jeroen Knoester <jeroenknoester@gmail.com>
2017-09-12 17:36:40 +0200

    Add rmd file, add answers to exercise 1.

---
+++ Statistiek_Exercises_Week_2/script.R
@@ -0,0 +1,30 @@
+x <- sample( 1:30, 98, replace = T )
+x
+x2 <- c(x, -5, 50)
+x2
+median(x) == median(x2)
+
+IQR(x)
+IQR(x2)
+
+h <- mean(x) + IQR(x) * 1.5
+l <- mean(x) - IQR(x) * 1.5
+h
+l
+# Check if there is any number in x that is higher then h or lower then l(the highest and lowest boundry)
+x[x > h]
+x[x < l]
+
+# Same for x2
+h2 <- mean(x2) + IQR(x2) * 1.5
+l2 <- mean(x2) - IQR(x2) * 1.5
+x2[x2 > h2]
+x2[x2 < l2]
+
+# Create boxplot
+boxplot(x, x2)
+# Shows the outliner +50 but not the outliner -5
+
+# show the standard deviation of both variables
+sd(x)
+sd(x2)
